cac769f825b225fbaf6e4b80dc9464c910ce05a4
chml <chmlaiii@gmail.com>
2015-06-30 02:01:45 +0800

    Add .ycm_extra_conf.py

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,3 +4,4 @@
 ln -s ~/.spf13-vim-local/.vimrc.local ~/.vimrc.local
 ln -s ~/.spf13-vim-local/.vimrc.before.local ~/.vimrc.before.local
 ln -s ~/.spf13-vim-local/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/.spf13-vim-local/.ycm_extra_conf.py ~/.ycm_extra_conf.py
